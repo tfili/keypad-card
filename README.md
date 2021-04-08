02017bc4ed229a61f2149dad5449e083457d4eef
Tom Fili <tom@cesium.com>
2021-04-07 21:52:18 -0400

    keypad-card works and has doc

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
-# keypad-card
-Keypad card
+Custom component for lovelace that guards a card behind a keypad. This can be useful for alarm systems that don't accept a code (eg. Abode) or for other cards that are sensitive.
+
+![Screenshot](/images/keypad.jpeg)
+
+## Config
+
+| Name | Type | Description | Default
+| ---- | ---- | ----------- | -------
+| type | string | `custom:keypad-card` | **Required**
+| card | object | The card to be guarded | **Required**
+| code | number | The code that must be entered to see `card` | **Required**
+| timeout | number | Number of seconds to show `card` before going back to the keypad (0 = _Show Forever_) | 0
+| exemptions | list | Specify `user` parameter with the user ID or `username` with the username where keypad won't be shown | []
+
+## Installation
+
+### Step 1
+
+Install `keypad-card` by copying `keypad-card.js` from this repo to `<config directory>/www/keypad-card/` on your Home Assistant instance.
+
+**Example:**
+
+```bash
+mkdir <config directory>/www/keypad-card/
+cd <config directory>/www/keypad-card/
+
+wget https://raw.githubusercontent.com/tfili/keypad-card/master/keypad-card.js
+```
+
+### Step 2
+
+Link `keypad-card` inside your `ui-lovelace.yaml`.
+
+```yaml
+resources:
+  - url: /local/keypad-card/keypad-card.js?v=0
+    type: module
+```
+
+Or add to `Configuration` -> `Lovelace Dashboards` -> `Resources`
+![Add Resource](/images/add-resource.jpeg)
+
+### Step 3
+
+Add as custom card of a panel view in your `ui-lovelace.yaml` using `type: custom:keypad-card`
+
+## Example
+```yaml
+type: custom:keypad-card
+code: 12345
+timeout: 10
+exemptions:
+  - user: b9f1f1d2a4ba406cba6739eece3ca1c6
+  - username: John Doe
+card:
+  type: alarm-panel
+  states:
+    - arm_home
+    - arm_away
+  entity: alarm_control_panel.my_alarm
+```
+
+## Thanks
+
+Thanks to all the people who have contributed!
+
+[![contributors](https://contributors-img.web.app/image?repo=tfili/keypad-card)](https://github.com/tfili/keypad-card/graphs/contributors)
---
+++ keypad-card.js
@@ -0,0 +1,160 @@
+const BUTTONS = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "", "0", "clear"];
+
+customElements.whenDefined("card-tools").then(() => {
+  var cardTools = customElements.get('card-tools');
+  const LitElement = cardTools.LitElement;
+  const html = cardTools.LitHtml;
+  const css = cardTools.LitCSS;
+
+  class KeypadCard extends LitElement {
+    static get properties() {
+      return {
+        hass: { type: Object },
+        code: { type: String },
+        card: { type: Object },
+        timeout: { type: Number },
+        exemptions: { type: Array },
+        _unlocked: { type: Boolean },
+      };
+    }
+
+    static get styles() {
+      return css`
+        paper-input {
+          margin: 0 auto 8px;
+          max-width: 150px;
+          text-align: center;
+        }
+
+        #keypad {
+          display: flex;
+          justify-content: center;
+          flex-wrap: wrap;
+          margin: auto;
+          width: 100%;
+          max-width: 300px;
+        }
+
+        #keypad mwc-button {
+          padding: 8px;
+          width: 30%;
+          box-sizing: border-box;
+        }
+
+        mwc-button.numberkey {
+          --mdc-typography-button-font-size: var(--keypad-font-size, 0.875rem);
+        }
+      `;
+    }
+
+    constructor() {
+      super();
+      this.hass = undefined;
+      this.name = "";
+      this.code = "";
+      this.card = undefined;
+      this.timeout = 0;
+      this.exemptions = []; 
+      this._input = undefined;
+      this._unlocked = false;
+      this._timer = undefined;
+    }
+
+    render() {
+      const exempt = this.exemptions.some(exemption => {
+        return this.hass?.user?.name === exemption.username ||
+        this.hass?.user?.id === exemption.user
+      });
+      if (exempt || this._unlocked) {
+        return html`
+          <ha-card>
+            <card-maker .config=${this.card} .hass=${this.hass}></card-maker>
+          </ha-card>
+        `;
+      }
+
+      return html`
+        <ha-card>
+          <h1 class="card-header">${this.name}</h1>
+          <paper-input
+            id="code"
+            type="password"
+            inputmode="numeric"
+          ></paper-input>
+          <div id="keypad">
+            ${BUTTONS.map((value) => {
+              return value === ""
+                ? html` <mwc-button disabled></mwc-button> `
+                : html`
+                    <mwc-button
+                      .value="${value}"
+                      @click="${this._handlePadClick}"
+                      outlined
+                      class=${value !== "clear" ? "numberkey" : ""}
+                      >${value}
+                    </mwc-button>
+                  `;
+            })}
+          </div>
+        </ha-card>
+      `;
+    }
+
+    updated() {
+      this._input = this.shadowRoot.getElementById("code");
+    }
+
+    _handlePadClick(e) {
+      const val = e.currentTarget.value;
+      this._input.value = val === "clear" ? "" : this._input.value + val;
+
+      if (this._input.value === this.code) {
+        this._input.value = "";
+        this._unlocked = true;
+
+        if(this._timer) {
+          clearTimeout(this._timer);
+        }
+
+        if (this.timeout > 0) {
+          this._timer = setTimeout(() => {
+            this._unlocked = false;
+            this._timer = undefined;
+          }, this.timeout);
+        }
+      }
+    }
+
+    setConfig(config) {
+      if (!config.code) {
+        throw new Error("You need to define a code");
+      }
+
+      if (!config.card) {
+        throw new Error("You need to define a card");
+      }
+
+      this.name = config.name || "";
+      this.code = config.code.toString();
+      this.card = config.card;
+      this.timeout = config.timeout ? config.timeout * 1000 : 0; // Seconds -> Milliseconds
+      this.exemptions = config.exemptions || [];
+    }
+  }
+
+  customElements.define("keypad-card", KeypadCard);
+});
+
+setTimeout(() => {
+  if (customElements.get("card-tools")) return;
+  customElements.define(
+    "keypad-card",
+    class extends HTMLElement {
+      setConfig() {
+        throw new Error(
+          "Can't find card-tools. See https://github.com/thomasloven/lovelace-card-tools"
+        );
+      }
+    }
+  );
+}, 2000);
